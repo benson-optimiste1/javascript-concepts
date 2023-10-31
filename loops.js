112a0f5920cd6dad4c3324efa065aaa550a84b00
benson-optimiste1 <chatelainevelybenson@gmail.com>
2023-10-31 17:00:29 -0400

    function fizzBuzz

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 // 1. for loop
 
-const students = [
+const studentsArray = [
     {id: 1, name: 'jhon', field: 'doctor'},
     {id: 2, name: 'fred', field: 'software engineer'},
     {id: 3, name: 'nive', field: 'avocat'},
@@ -11,20 +11,38 @@ const students = [
 ]
 
 // 1.for loop
-for (let i =0; i < students.length; i= i+1){
-// console.log(students[i])
-}
+// for (let i =0; i < studentsArray.length; i= i+1){
+// console.log(studentsArray[i])
+// }
 
-2. //for each
-// students.forEach((item, index) => {
+ // 2.for each
+// studentsArray.forEach((item, index) => {
 //     console.log(index, '->', item)
 // })
 
 // 3. for map
 
-const newArray = students.map((item, index) =>{
-    console.log(index, '->', item)
-    item.age = index + 10 
-    return item
-})
+// const newArray = studentsArray.map((item, index) =>{
+    // console.log(index, '->', item)
+    // item.age = index + 10 
+    // return `<h2> item${item.name, item.field}</h2>`
+// })
 
+// 3.1 map one liner
+// studentsArray.map((`<h2> item${item.name, item.field}</h2>`))
+
+// loop on an array and if id is a multiple of 3 console fizz
+// if id is multiple of 5 console buzz
+function fizzBuzz(){
+    studentsArray.map((item, index) =>{
+        if (item.id % 3 === 0 && item.id % 5 === 0){
+            return 'fizzBuzz'
+        } else if (item.id % 5 === 0){
+            return 'buzz'
+        } else if (item.id % 3 === 0){
+            return 'fizz'
+        } else {
+            return 'not divisible by 3 or 5'
+        }
+    })
+}
